df50c54c63657ed267be252229d3085cf8beb2e1
Jacob Rienstra <jacobdrienstra@gmail.com>
2020-03-21 13:10:52 -0400

    tweaks for typing and preloading (#1)

--- src/layouts/PageGlobal.react.tsx
+++ src/layouts/PageGlobal.react.tsx
@@ -12,6 +12,8 @@ function Page({ children }: WithChildren): JSX.Element {
       <Helmet>
         <title>Satire V</title>
         <link rel="preconnect" href="https://platform.twitter.com" />
+        <link rel="preconnect" href="https://syndication.twitter.com" />
+        <link rel="preconnect" href="https://cdn.syndication.twimg.com" />
         <link rel="preconnect" href="https://pbs.twimg.com" />
         <link
           rel="preload"
